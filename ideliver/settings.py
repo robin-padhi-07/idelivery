fb434a85426c8239e13b655c87ac9e47276b9f7d
Robin <32897617+robin-padhi-07@users.noreply.github.com>
2020-05-21 12:53:01 +0530

    Update settings.py

--- ideliver/settings.py
+++ ideliver/settings.py
@@ -126,10 +126,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-STATIC_DIR,
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 LOGIN_URL = '/base_app/user_login'
 # AUTH_USER_MODEL = 'base_app.User'
